7141a7f785970fed0e161e4b6ed90d4772aefa2d
Maruf Hasan Shoaib <122290505+SHOAIB-truenO@users.noreply.github.com>
2023-04-10 22:19:02 +0600

    Create 41A-261-50-07.cpp

---
+++ Lab 11/41A-261-50-07.cpp
@@ -0,0 +1,24 @@
+// 41A - Translation	 
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
